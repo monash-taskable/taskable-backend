53d3a0ff8ea393c95fb1fc8e6f775966508ce5e7
PinkHouseTheory <andy.ouyang131@gmail.com>
2024-10-07 16:16:52 +1100

    add dot-env dependency to handle environment variables

--- src/main/java/com/taskable/backend/BackendApplication.java
+++ src/main/java/com/taskable/backend/BackendApplication.java
@@ -1,11 +1,14 @@
 package com.taskable.backend;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BackendApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+
 		SpringApplication.run(BackendApplication.class, args);
 	}
 
